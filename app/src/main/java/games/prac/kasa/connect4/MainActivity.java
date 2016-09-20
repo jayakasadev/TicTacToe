d0767bd67c136cb46dd75dde51a7be7327165d7e
Jaya Kasa <kasa288@gmail.com>
2016-09-20 15:03:43 -0400

    working on dropping coin in

--- app/src/main/java/games/prac/kasa/connect4/MainActivity.java
+++ app/src/main/java/games/prac/kasa/connect4/MainActivity.java
@@ -2,8 +2,27 @@ package games.prac.kasa.connect4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean turn = false;
+
+    public void dropIn(View view){
+        ImageView coin = (ImageView) view;
+        coin.setTranslationY(-1000f);
+        if(turn){
+            coin.setImageResource(R.drawable.blue);
+            turn = false;
+        }
+        else{
+            coin.setImageResource(R.drawable.red);
+            turn = true;
+        }
+        coin.animate().translationYBy(1000f).setDuration(300);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
