f01a179742255851b4aa35f00453dba5bbaca086
Jaya Kasa <kasa288@gmail.com>
2016-09-21 12:20:50 -0400

    working on displaying and calculating the winner

--- app/src/main/java/games/prac/kasa/connect4/MainActivity.java
+++ app/src/main/java/games/prac/kasa/connect4/MainActivity.java
@@ -3,19 +3,65 @@ package games.prac.kasa.connect4;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    //false = yellow
+    //true = red
+    private boolean activePlayer = false;
+
+    private boolean[] gameState = new boolean[9];
+
+    private int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6 ,7 ,8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
+    /**
+     * Method handles dropping in new coins and checking who won.
+     *
+     * @param view
+     */
     public void dropIn(View view){
         ImageView coin = (ImageView) view;
-        coin.setTranslationY(-1000f);
-        coin.setImageResource(R.drawable.red);
-        coin.animate().translationYBy(1000f).setDuration(300);
+        //coin.setImageAlpha(100);
+
+        Log.d("tag", coin.getTag().toString());
+        int tag = Integer.parseInt(coin.getTag().toString());
+        //Log.d("alpha", ""+coin.getImageAlpha());
+        //coin.setVisibility(View.VISIBLE);
+        if(!gameState[tag]){
+            if(activePlayer) {
+                coin.setImageResource(R.drawable.red);
+                activePlayer = false;
+            }
+            else{
+                coin.setImageResource(R.drawable.yellow);
+                activePlayer = true;
+            }
+            gameState[tag] = true;
+
+            coin.setTranslationY(-5000f);
+            coin.animate().translationYBy(5000f).setDuration(300);
+
+            for(int[] win : winningPositions){
+                if(gameState[win[0]] && gameState[win[1]] && gameState[win[2]]){
+                    Log.d("win", ""+ activePlayer);
+
+                    //Someone one
+                    LinearLayout linearLayout = (LinearLayout)findViewById(R.id.newGameLayout);
+                    linearLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+    }
+
+    public void playAgain(View view){
+
     }
 
     @Override
@@ -23,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //this code is custom and not what the lecturer discusses in the lecture
+        /*
         TableLayout tableLayout = (TableLayout)findViewById(R.id.grid);
 
         Display display = getWindowManager().getDefaultDisplay();
@@ -30,10 +78,12 @@ public class MainActivity extends AppCompatActivity {
         display.getSize(size);
         int width = size.x;
 
-        System.err.println("Width is " + width);
+        //System.out.println("\n\nWidth is " + width + "\n\n");
+        Log.d("width", ""+width);
 
         tableLayout.getLayoutParams().height = width;
         tableLayout.getLayoutParams().width = width;
         tableLayout.requestLayout();
+        */
     }
 }
