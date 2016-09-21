1f3e7eb4b0609d391f83793a4c0f0beda49cc3d9
Jaya Kasa <kasa288@gmail.com>
2016-09-21 13:07:17 -0400

    app works working on animations

--- app/src/main/java/games/prac/kasa/connect4/MainActivity.java
+++ app/src/main/java/games/prac/kasa/connect4/MainActivity.java
@@ -1,25 +1,26 @@
 package games.prac.kasa.connect4;
 
-import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    //false = yellow
-    //true = red
+    //false = yellow = 0
+    //true = red = 1
     private boolean activePlayer = false;
 
-    private boolean[] gameState = new boolean[9];
+    private int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
     private int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6 ,7 ,8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
+    private boolean reset = false;
 
     /**
      * Method handles dropping in new coins and checking who won.
@@ -30,37 +31,85 @@ public class MainActivity extends AppCompatActivity {
         ImageView coin = (ImageView) view;
         //coin.setImageAlpha(100);
 
-        Log.d("tag", coin.getTag().toString());
+        //Log.d("tag", coin.getTag().toString());
         int tag = Integer.parseInt(coin.getTag().toString());
         //Log.d("alpha", ""+coin.getImageAlpha());
         //coin.setVisibility(View.VISIBLE);
-        if(!gameState[tag]){
+        if(gameState[tag] == 2){
             if(activePlayer) {
                 coin.setImageResource(R.drawable.red);
                 activePlayer = false;
+                gameState[tag] = 1;
             }
             else{
                 coin.setImageResource(R.drawable.yellow);
                 activePlayer = true;
+                gameState[tag] = 0;
             }
-            gameState[tag] = true;
 
-            coin.setTranslationY(-5000f);
-            coin.animate().translationYBy(5000f).setDuration(300);
+            coin.setTranslationY(-7500f);
+            coin.animate().translationYBy(7500f).setDuration(300);
 
             for(int[] win : winningPositions){
-                if(gameState[win[0]] && gameState[win[1]] && gameState[win[2]]){
-                    Log.d("win", ""+ activePlayer);
+                if(gameState[win[0]] == gameState[win[1]] && gameState[win[1]] == gameState[win[2]] && gameState[win[1]] != 2){
+                    //Log.d("win", ""+ activePlayer);
 
-                    //Someone one
+                    //fade the game out of view
+                    GridLayout g = (GridLayout)findViewById(R.id.grid);
+                    //g.setAlpha(0);
+                    g.animate().alpha(0).setDuration(750);
+
+                    //Someone won
                     LinearLayout linearLayout = (LinearLayout)findViewById(R.id.newGameLayout);
                     linearLayout.setVisibility(View.VISIBLE);
+                    linearLayout.animate().alpha(1).setDuration(750);
+
+                    //let player know who won
+                    TextView text = (TextView)findViewById(R.id.winnerMessage);
+                    String message = "";
+                    if(gameState[win[0]] == 0){
+                        //yellow won
+                        message = "Yellow Wins!";
+                    }
+                    else{
+                        //red won
+                        message = "Red Wins!";
+                    }
+                    text.setText(message);
+
+                    break;
                 }
             }
         }
     }
 
     public void playAgain(View view){
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.newGameLayout);
+        linearLayout.animate().alpha(0).setDuration(750);
+        linearLayout.setVisibility(View.INVISIBLE);
+
+        activePlayer = false;
+
+        for(int a = 0; a < gameState.length; a++){
+            gameState[a] = 2;
+        }
+
+        GridLayout grid = (GridLayout)findViewById(R.id.grid);
+
+        for(int a = 0; a < grid.getChildCount(); a++){
+            //Log.d("child", ""+a);
+
+            LinearLayout linear = (LinearLayout)grid.getChildAt(a);
+            for(int b = 0; b < linear.getChildCount(); b++){
+                ((ImageView)linear.getChildAt(b)).setImageResource(0);
+            }
+
+        }
+
+        //fade the game out of view
+        GridLayout g = (GridLayout)findViewById(R.id.grid);
+        //g.setAlpha(0);
+        g.animate().alpha(1).setDuration(750);
 
     }
 
